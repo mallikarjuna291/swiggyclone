b95e707d52d593ae0a3732790e645dc96441b77c
lankamallikarjuna <lankamallikarjuna001@gmail.com>
2022-07-22 18:45:20 +0530

    changed design

--- src/CustomerView.js
+++ src/CustomerView.js
@@ -3,15 +3,18 @@ import { DISHES_QUERY } from "./Queries/DISHES_QUERY";
 
 import { useNavigate } from "react-router-dom";
 
-import { Button } from "react-bootstrap";
 
 import { INSERT_ORDERS } from "./Queries/INSERT_ORDERS";
 import { ORDERS_QUERY } from "./Queries/ORDERS_QUERY";
 import { USERS_QUERY } from "./Queries/USERS_QUERY";
 import { RESTAURANTS_QUERY } from "./Queries/RESTAURANTS_QUERY";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
-import Orders from "./Orders";
 const CustomerView = () => {
   const navigate = useNavigate();
   const uri = window.location;
@@ -95,29 +98,45 @@ const statusdata=openedRestaurants?.map((each)=>{
     navigate(`/customer/${userid}/orders`)
   } 
   const pages = ["Orders"]
-  
   return (
     <>
      <Navbar pages={pages} handleClick={goToOrders}/>
-  
-      <div style={{ display: "flex", margin: "5px" ,marginTop:'30px'}}>
-          <h6 style={{ width: '24%'}}>S.No</h6>
-          <h6 style={{ width:'24%' }}>Item Name</h6>
-          <h6 style={{ width: '24%'}}>Description</h6>
-          <h6 style={{ width: '24%' }}>Price</h6>
-         
-        </div>
-      { filteredByValue?.map((value, index) => (
-        <div style={{ display: "flex", margin: "5px" }}>
-          <p style={{ flex: 1 }}>{index + 1}</p>
-          <p style={{ flex: 1 }}>{value.Name}</p>
-          <p style={{ flex: 1 }}>{value.Description}</p>
-          <p style={{ flex: 1 }}>{value.Price}</p>
-         
-          <Button
+    
+     {filteredByValue?.map((value)=>{
+     return  <Card  sx={{ minWidth: 150 }}>
+      <CardContent>
+      <div style={{display:'flex'}}>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{margin:'10px',marginRight:'60px'}}>
+      Name
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{margin:'10px'}}>
+        {value.Name}
+        </Typography>
+      </div>
+       
+      <div style={{display:'flex'}}>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{margin:'10px',marginRight:'23px'}}>
+      Description
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{margin:'10px'}}>
+          {value.Description}
+        </Typography>
+      </div>
+      <div style={{display:'flex'}}>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{margin:'10px',marginRight:'60px'}}>
+      Price
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{margin:'10px'}}>
+         {value.Price}
+        </Typography>
+      </div>
+      </CardContent>
+      <CardActions>
+      <Button
             variant="outline-primary"
             type="submit"
             value={value.Restaurantid}
+            style={{color:'blue'}}
             onClick={(event) =>
               handleClick(
                 event,
@@ -131,8 +150,9 @@ const statusdata=openedRestaurants?.map((each)=>{
           >
             Order
           </Button>
-        </div>
-      ))}
+      </CardActions>
+    </Card>
+     })}
     </>
   );
 };
