26099176f41a37db5f70d814d19f0de5903041b5
lankamallikarjuna <lankamallikarjuna001@gmail.com>
2022-08-04 13:06:07 +0530

    added logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,8 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
-import image1 from "./Images/image1.png";
-const settings = ["Logout"];
+import image3 from "./Images/image1.png";
+
 
 const Navbar = (props) => {
   const navigate = useNavigate();
@@ -20,32 +15,38 @@ const Navbar = (props) => {
   const handleClick = props.handleClick;
 
   return (
-    <AppBar position="static" sx={{ bgcolor: "#FBB454" }}>
-      <Container style={{ margin: 0 }}>
-        <Toolbar>
-          <img src={image1} style={{ width: "8%", marginRight: "7%" }} />
+    <div
+      style={{
+        margin: 0,
+        display: "flex",
+        height: "80px",
+        backgroundColor: "#6E85B7",
+      }}
+    >
+      <img src={image3} style={{ width: "9%", marginRight: "6%" }} />
 
-          <Box sx={{ flexGrow: 1 }}>
-            {pages?.map((page) => (
-              <Button
-                key={page}
-                onClick={handleClick}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+      {pages?.map((page) => (
+        <Button
+          key={page}
+          onClick={handleClick}
+          style={{ flex: 2, color: "white", justifyContent: "start" }}
+        >
+          {page}
+        </Button>
+      ))}
 
-          <Button
-            sx={{ color: "white", marginRight: "-30%" }}
-            onClick={handleclose}
-          >
-            Log out
-          </Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+      <Button
+        style={{
+          flex: 1,
+          color: "white",
+          justifyContent: "end",
+          paddingRight: "2%",
+        }}
+        onClick={handleclose}
+      >
+        Log out
+      </Button>
+    </div>
   );
 };
 export default Navbar;
